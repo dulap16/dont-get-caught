dffaf852af9085e900615bbdc2f4ce1ef6537d55
dulap16 <tudorbrk8@yahoo.com>
2023-09-16 19:29:15 +0300

    Add change of color when kissing (temporary)

--- kiss/Assets/Scripts/Kissers.cs
+++ kiss/Assets/Scripts/Kissers.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Kissers : MonoBehaviour
 {
     [SerializeField] private bool areKissing = false;
+    private SpriteRenderer sr;
+    private Color initColor;
+    [SerializeField] private Color kissingColor;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        initColor = sr.color;
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
@@ -14,6 +24,24 @@ public class Kissers : MonoBehaviour
         }
         else areKissing = false;
     }
+
+    private void setColor(Color c)
+    {
+        sr.color = c;
+    }
+
+    private void kissing()
+    {
+        areKissing = true;
+        setColor(kissingColor);
+    }
+
+    private void stoppedKissing()
+    {
+        areKissing = false;
+        setColor(initColor);
+    }
+
     public bool getAreKissing()
     {
         return areKissing;
