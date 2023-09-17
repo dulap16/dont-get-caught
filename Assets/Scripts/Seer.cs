cecead0f6a5c0558ed975e9b8e31161546430525
dulap16 <tudorbrk8@yahoo.com>
2023-09-17 20:58:21 +0300

    Add methods for changing states

--- Assets/Scripts/Seer.cs
+++ Assets/Scripts/Seer.cs
@@ -47,10 +47,30 @@ public class Seer : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartWarning()
     {
-        
+        state = 1;
+        sr.color = new Color(255, 255, 0);
+
+        StartCoroutine(WaitUntilStartSeeing(nextWarningTime));
+    }
+
+    private void StartSeeing()
+    {
+        state = 2;
+        sr.color = new Color(255, 0, 0);
+
+        StartCoroutine(WaitUntilStopSeeing(seeingTime));
+    }
+
+    private void StopSeeing()
+    {
+        state = 0;
+        sr.color = new Color(0, 255, 0);
+
+        StartCoroutine(WaitUntilWarning(timeBetweenSees));
+    }
+
     public bool isIdle()
     {
         return state == 0;
