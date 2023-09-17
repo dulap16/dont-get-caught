845631ececfaad1b2831cc44fdb6ca1b5a8b61ce
dulap16 <tudorbrk8@yahoo.com>
2023-09-17 21:00:04 +0300

    Add manager script

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] private Kissers kissers;
+    [SerializeField] private Seer seer;
+    [SerializeField] private GameObject kissingBar;
+
+    [SerializeField] private float ratio = 0.1f;
+    [SerializeField] private float currentProgress;
+    [SerializeField] private float finish = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(kissers.getAreKissing())
+        {
+            if (seer.isSeeing())
+                Debug.Log("You were caught");
+            else
+            {
+                currentProgress = currentProgress + ratio;
+                if (currentProgress >= finish)
+                {
+                    Debug.Log("finished");
+                }
+            }
+        }
+    }
+}
